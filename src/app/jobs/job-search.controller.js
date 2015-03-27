4ab946e1505f3e3b1d35391c801c94309e4757b5
LHBDev <freeraprb@gmail.com>
2015-03-26 19:54:41 -0500

    Finished jobs controller

--- src/app/jobs/job-search.controller.js
+++ src/app/jobs/job-search.controller.js
@@ -4,7 +4,11 @@
 
 'use strict';
 
-angular.module('bananafish.jobs').controller('JobSearchCtrl', function ($scope) {
+angular.module('bananafish.jobs').controller('JobSearchCtrl', function (Jobs) {
 
   var vm = this;
+
+  Jobs.getJobs().then(function (jobs) {
+    vm.jobs = jobs;
+  });
 });
