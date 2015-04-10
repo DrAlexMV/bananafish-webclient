ea2e54cca54cefa853c48fb1c12ecf7b20cecf41
LHBDev <freeraprb@gmail.com>
2015-04-09 20:20:32 -0500

    fix tests

--- src/app/tests/test-search.controller.js
+++ src/app/tests/test-search.controller.js
@@ -9,7 +9,7 @@ angular.module('bananafish.tests').controller('TestSearchCtrl', function($http){
   var vm = this;
 
   $http.get("http://23.253.89.46:5000/api/v1/tests/").success(function(response){
-    vm.results = response;
+    vm.results = response.test_result;
     console.log(response.test_result);
   });
 });
