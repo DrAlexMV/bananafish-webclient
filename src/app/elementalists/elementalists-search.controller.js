3d7c6ea7a6ffa7ea62e7b9d8a5428a21e3915cb9
Steven Zvonek <skz94@cs.utexas.edu>
2015-04-16 15:17:37 -0500

    Update elementalists-search.controller.js

--- src/app/elementalists/elementalists-search.controller.js
+++ src/app/elementalists/elementalists-search.controller.js
@@ -8,7 +8,7 @@ angular.module('bananafish.elementalists').controller('ElemSearchCtrl', function
 
   var vm = this;
 
-  $http.get("http://private-anon-0cec3ef3e-periodictable.apiary-mock.com/api/element").success(function(response){
+  $http.get("http://private-8bae8-periodictable.apiary-mock.com/api/element").success(function(response){
     vm.results = response.data;
     console.log(response.data);
   }).error(function(response){
