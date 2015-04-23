af8cb695cc9d680d1491784af8edd7a626246eb7
LHBDev <freeraprb@gmail.com>
2015-04-23 03:04:50 -0500

    rollback navbar

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -3,8 +3,4 @@
 angular.module('bananafish')
   .controller('NavbarCtrl', function ($scope) {
     $scope.date = new Date();
-    $scope.isActive = function (viewLocation){
-      var active = (viewLocation === $location.path());
-      return active;
-    };
   });
