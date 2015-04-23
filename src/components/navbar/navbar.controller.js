600debeb52fd7d08db1192f2b8d5d0e64a86f0fb
LHBDev <freeraprb@gmail.com>
2015-04-23 03:03:33 -0500

    Navbar update

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -3,4 +3,8 @@
 angular.module('bananafish')
   .controller('NavbarCtrl', function ($scope) {
     $scope.date = new Date();
+    $scope.isActive = function (viewLocation){
+      var active = (viewLocation === $location.path());
+      return active;
+    };
   });
