9225271e8e4e443da942c12399020ef43e7c3080
Steven Zvonek <skz94@cs.utexas.edu>
2015-04-24 18:24:14 -0500

    Update elementalists-search.controller.js

--- src/app/elementalists/elementalists-search.controller.js
+++ src/app/elementalists/elementalists-search.controller.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-angular.module('bananafish.elementalists').controller('ElemSearchCtrl', function(){
+angular.module('bananafish.elementalists').controller('ElemSearchCtrl', function($http){
 
   var vm = this;
   
