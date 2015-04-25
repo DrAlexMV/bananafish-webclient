9d0793dd4abc647f89af4430924038b2caebfeab
LHBDev <freeraprb@gmail.com>
2015-04-24 21:18:48 -0500

    6

--- src/app/index.js
+++ src/app/index.js
@@ -21,7 +21,7 @@ angular.module('bananafish', [
         controller: 'MainCtrl as main'
       });
 
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/#');
 
     // Restangular configuration
 
